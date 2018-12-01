3e5f01edcf6cc10ab77606594adaaf063fd891ab
RileyMcGee <riley.mcgee.13@gmail.com>
2018-12-01 13:47:33 -0600

    framework for aoc days

---
+++ src/main/groovy/aoc/AoCDay.groovy
@@ -0,0 +1,6 @@
+package aoc
+
+interface AoCDay {
+    def part1(input)
+    def part2(input)
+}
---
+++ src/main/groovy/aoc/day/Day01.groovy
@@ -0,0 +1,15 @@
+package aoc.day
+
+import aoc.AoCDay
+
+class Day01 implements AoCDay {
+    @Override
+    def part1(Object input) {
+        return null
+    }
+
+    @Override
+    def part2(Object input) {
+        return null
+    }
+}
